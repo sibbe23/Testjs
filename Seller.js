dd85c90b97aeac74705227042a8cbf9f3827e948
sibbe23 <sibbe23@gmail.com>
2023-02-02 14:21:07 +0530

    ok

--- Seller.js
+++ Seller.js
@@ -1,14 +1,18 @@
  function myfunction(event){
    event.preventDefault();
-    const s_price = event.target.amount.value;
+
+   //taking the value from the input
+    const s_price = event.target.amount.value; 
     const p_name = event.target.name.value;
     const cat = event.target.items.value;
     console.log(s_price,p_name,cat);
 
+    //storing in local storage
     localStorage.setItem('amount',s_price)
     localStorage.setItem('name',p_name)
     localStorage.setItem('items',cat)
 
+    //creating an object with the inputs
     const obj ={
         s_price,
         p_name,
@@ -17,6 +21,7 @@
     localStorage.setItem('userdetails',JSON.stringify(obj))
     showOnScreen(obj)
 
+    //using async await function 
     async function createPost(url, obj) {
         try {      
          const response = await fetch(url, {      
@@ -30,96 +35,65 @@
          console.error(error);  
         } 
        }
- createPost("https://crudcrud.com/api/0a8313bff0cf4f2dbe450c3c1e303544/sellers",obj);
+ createPost("https://crudcrud.com/api/3de0cf8c7a314de883a6de0452481d64/sellers",obj); //creating the sellers in crudcrud
  }
- function showOnScreen(obj){
+ function showOnScreen(obj){ //function to show the contents on screen
+
+    function showUnderTitles(id){ //function to show each values under their respective titles 
+        const parentelem = id;
+        const childelem = document.createElement('li');
+        childelem.textContent = obj.s_price +'-'+obj.p_name+'-'+obj.cat;
+        parentelem.appendChild(childelem);
+        const deletebutton = document.createElement('input');
+        deletebutton.type="button";
+        deletebutton.value="Delete Order";
+        deletebutton.className="btn btn-danger btn-sm"
+        deletebutton.onclick=()=>{
+            axios.delete(`https://crudcrud.com/api/3de0cf8c7a314de883a6de0452481d64/sellers/${obj._id}`)
+            .then((response)=>{
+                
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+            localStorage.removeItem(obj.cat)
+            parentelem.removeChild(childelem);           
+        }
+        const editButton = document.createElement('input')
+        editButton.type = "button"
+        editButton.value="Edit"
+        editButton.className="btn btn-outline-secondary btn-sm"
+        editButton.onclick=()=>{         
+            localStorage.removeItem(obj.childelem)
+            parentelem.removeChild(childelem)
+            document.getElementById('amount').value=obj.s_price;
+            document.getElementById('name').value = obj.p_name;
+            document.getElementById('items').value = obj.cat;
+        }
+        childelem.appendChild(deletebutton);
+        childelem.appendChild(editButton);
+        parentelem.appendChild(childelem);
+     }
    
- if(obj.cat == 'Electronics'){
-    const parentelem1 = document.getElementById('item1');
-    const childelem1 = document.createElement('li');
-    childelem1.textContent = obj.s_price +'-'+obj.p_name+'-'+obj.cat;
-    parentelem1.appendChild(childelem1);
-    const deletebutton1 = document.createElement('input');
-    deletebutton1.type="button";
-    deletebutton1.value="Delete Order";
-    deletebutton1.onclick=()=>{
-        localStorage.removeItem(obj.cat)
-        parentelem1.removeChild(childelem1);
-        
-    }
-    const editButton1 = document.createElement('input')
-    editButton1.type = "button"
-    editButton1.value="Edit"
-    editButton1.onclick=()=>{
-        localStorage.removeItem(obj.childelem1)
-        parentelem1.removeChild(childelem1)
-        document.getElementById('amount').value=obj.s_price;
-        document.getElementById('name').value = obj.p_name;
-        document.getElementById('items').value = obj.cat;
-    }
-    childelem1.appendChild(deletebutton1);
-    childelem1.appendChild(editButton1);
-    parentelem1.appendChild(childelem1);
+ if(obj.cat == 'Electronics'){ //getting element by category
+   let items1 = document.getElementById('item1')
+    showUnderTitles(items1); //item1
 }
  
-   else if(obj.cat == 'Food'){ const parentelem2 = document.getElementById('item2');
-    const childelem2 = document.createElement('li');
-    childelem2.textContent = obj.s_price +'-'+obj.p_name+'-'+obj.cat;
-    parentelem2.appendChild(childelem2);
-    const deletebutton2 = document.createElement('input');
-    deletebutton2.type="button";
-    deletebutton2.value="Delete Order";
-    deletebutton2.onclick=()=>{
-        localStorage.removeItem(obj.cat)
-        parentelem2.removeChild(childelem2);
-    }
-    const editButton2 = document.createElement('input')
-    editButton2.type = "button"
-    editButton2.value="Edit"
-    editButton2.onclick=()=>{
-        localStorage.removeItem(obj.childelem2)
-        parentelem2.removeChild(childelem2)
-        document.getElementById('amount').value=obj.s_price;
-        document.getElementById('name').value = obj.p_name;
-        document.getElementById('items').value = obj.cat;
-    }
-    childelem2.appendChild(deletebutton2);
-    childelem2.appendChild(editButton2);
-    parentelem2.appendChild(childelem2);
+   else if(obj.cat == 'Food'){ //getting element by category
+let items2 = document.getElementById('item2')
+    showUnderTitles(items2); //item2
  }
 
-   else {const parentelem3 = document.getElementById('item3');
-    const childelem3 = document.createElement('li');
-    childelem3.textContent = obj.s_price +'-'+obj.p_name+'-'+obj.cat;
-    parentelem3.appendChild(childelem3);
-    const deletebutton3 = document.createElement('input');
-    deletebutton3.type="button";
-    deletebutton3.value="Delete Order";
-    deletebutton3.onclick=()=>{
-        localStorage.removeItem(obj.cat)
-        parentelem3.removeChild(childelem3);
-    }
-    const editButton3 = document.createElement('input')
-    editButton3.type = "button"
-    editButton3.value="Edit"
-    editButton3.onclick=()=>{
-        localStorage.removeItem(obj.childelem3)
-        parentelem3.removeChild(childelem3)
-        document.getElementById('amount').value=obj.s_price;
-        document.getElementById('name').value = obj.p_name;
-        document.getElementById('items').value = obj.cat;
-    }
-    childelem3.appendChild(deletebutton3);
-    childelem3.appendChild(editButton3);
-    parentelem3.appendChild(childelem3);
-
+   else { //getting element by category
+    let items3 = document.getElementById('item3')
+   showUnderTitles(items3); //items3
 }
     
-    
  }
 
- window.addEventListener("DOMContentLoaded",()=>{
-    const data = axios.get("https://crudcrud.com/api/0a8313bff0cf4f2dbe450c3c1e303544/sellers")
+ window.addEventListener("DOMContentLoaded",()=>{ //backend data from crudcrud
+    const data = axios.get("https://crudcrud.com/api/3de0cf8c7a314de883a6de0452481d64/sellers")
     .then((response)=>{
         console.log(response);
         for(let i =0;i<response.data.length;i++)
@@ -132,3 +106,5 @@
     })
     
  })
+
+ 
